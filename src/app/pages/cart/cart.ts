89267224976916c10732fec8555d523b2708bc42
lazo <lwachira742@gmail.com>
2025-07-01 17:53:30 +0300

    implementation

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart';
 import { CartItem } from "./cart-item/cart-item";
+import { OrderSummary } from "./order-summary/order-summary";
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItem],
+  imports: [CartItem, OrderSummary],
   template: `<div class="flex flex-col gap-4 p-6">
       <h2 class="text-2xl font-bold">Shopping Cart</h2>
       @for (item of cartService.cart(); track item.id)  {
      <app-cart-item [item]="item" />
       }
-      </div>`,
+      </div>
+        <app-order-summary />
+        `,
   styleUrl: './cart.scss'
 })
 export class Cart {
